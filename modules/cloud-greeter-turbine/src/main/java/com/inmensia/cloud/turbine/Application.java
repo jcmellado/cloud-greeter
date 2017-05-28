e2a40f01bcf0506cd71f7a680dcfc12a388844b9
jcmellado <jcmellado@gmail.com>
2017-05-28 10:56:59 +0200

    Added Turbine project

---
+++ modules/cloud-greeter-turbine/src/main/java/com/inmensia/cloud/turbine/Application.java
@@ -0,0 +1,16 @@
+package com.inmensia.cloud.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@EnableHystrixDashboard
+@SpringBootApplication
+public class Application {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
